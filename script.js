a2b7b26f13b0bdaf4d2697939620c9edbf931c05
lariany duarte <lariany.martins@aluno.ce.gov.br>
2025-08-12 12:48:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let nome = "lariany"
+let idade = 16
+let anodenascimento = 2009
+let cidade = "tejuçuoca"
+let profissao = "medica"
+
+let salario = 10000
+let bonus = 900
+let total = salario+bonus
+let mensal =  total
+let anual = mensal*12
+let dobro = anual*2
+alert (`nome: ${nome}
+idade:${idade}
+ano de nascimento: ${anodenascimento}
+cidade: ${cidade}
+profissao: ${profissao}
+salario: ${salario}
+bonus: ${bonus}
+total: ${total}
+mensal: ${mensal}
+anual: ${anual}
+dobro: ${dobro}
+`)
